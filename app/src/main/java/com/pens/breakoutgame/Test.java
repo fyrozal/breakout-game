7bbc1dd3c6c75f2635ad626a92fc2f6921d2db5c
Zulhaydar Akbar <fyrozal.akbar@gmail.com>
2020-12-15 17:01:37 +0700

    initial

---
+++ app/src/main/java/com/pens/breakoutgame/Test.java
@@ -0,0 +1,12 @@
+package com.pens.breakoutgame;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("tes");
+
+        int j = 40;
+        int l = 10;
+        int u = j+l;
+        System.out.println(""+u);
+    }
+}
